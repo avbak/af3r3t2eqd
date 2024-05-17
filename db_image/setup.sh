5012114d2f5428c9f3a57b38a696aed880ac1da7
avbak <101019899+avbak@users.noreply.github.com>
2024-05-18 00:29:10 +0300

    Update setup.sh
    Смена порядка команд с учетом переноса части из них в Dockerfile и .sql

--- db_image/setup.sh
+++ db_image/setup.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-mkdir /var/lib/postgresql/data/archive
-chown postgres:postgres /var/lib/postgresql/data/archive/
 echo "host replication $DB_REPL_USER 0.0.0.0/0 trust" >> /var/lib/postgresql/data/pg_hba.conf
-psql -c "CREATE DATABASE $DB_DATABASE;"
+psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
+psql -c "CREATE DATABASE $DB_DATABASE OWNER $DB_USER;"
 psql -d $DB_DATABASE -a -f /docker-entrypoint-initdb.d/00_init.sql
 psql -c "CREATE USER $DB_REPL_USER WITH REPLICATION LOGIN PASSWORD '$DB_REPL_PASSWORD';"
-psql -c "ALTER SYSTEM SET log_replication_commands TO 'on';"
-psql -c "ALTER SYSTEM SET archive_mode TO 'on';"
-psql -c "ALTER SYSTEM SET archive_command TO 'cp %p /var/lib/postgresql/data/archive/%f';"
-psql -c "ALTER SYSTEM SET max_wal_senders TO 10;"
-psql -c "ALTER SYSTEM SET wal_level TO 'replica';"
-psql -c "ALTER SYSTEM SET wal_log_hints TO 'on';"
+psql -d $DB_DATABASE -c "ALTER TABLE phones OWNER TO $DB_USER;"
+psql -c $DB_DATABASE -c "ALTER TABLE emails OWNER TO $DB_USER;"
 psql -c "SELECT pg_reload_conf();"
