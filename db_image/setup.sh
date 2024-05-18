351079bb371424e7fff1890ce40887fd5de1f2ae
avbak <101019899+avbak@users.noreply.github.com>
2024-05-18 14:03:12 +0300

    Update setup.sh
    Перенос конфигов в .sh

--- db_image/setup.sh
+++ db_image/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
 echo "host replication $DB_REPL_USER 0.0.0.0/0 trust" >> /var/lib/postgresql/data/pg_hba.conf
+psql -c "ALTER SYSTEM SET log_replication_commands TO 'on';"
+psql -c "ALTER SYSTEM SET archive_mode TO 'on';"
+psql -c "ALTER SYSTEM SET archive_command TO 'cp %p /postgres_archive/%f';"
+psql -c "ALTER SYSTEM SET max_wal_senders TO 10;"
+psql -c "ALTER SYSTEM SET wal_level TO 'replica';"
+psql -c "ALTER SYSTEM SET wal_log_hints TO 'on';"
 psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
 psql -c "CREATE DATABASE $DB_DATABASE OWNER $DB_USER;"
-psql -d $DB_DATABASE -a -f /docker-entrypoint-initdb.d/00_init.sql
+psql -d $DB_DATABASE -a -f /var/lib/postgresql/00_init.sql
 psql -c "CREATE USER $DB_REPL_USER WITH REPLICATION LOGIN PASSWORD '$DB_REPL_PASSWORD';"
 psql -d $DB_DATABASE -c "ALTER TABLE phones OWNER TO $DB_USER;"
 psql -c $DB_DATABASE -c "ALTER TABLE emails OWNER TO $DB_USER;"
