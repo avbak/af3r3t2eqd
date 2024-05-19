01e6b4c57f18c3acc98fba153c8631629d27b6ec
avbak <101019899+avbak@users.noreply.github.com>
2024-05-19 14:30:44 +0300

    Update setup.sh
    Исправлена опечатка и добавлена пауза между созданием БД, инициализацией и назначением прав

--- db_image/setup.sh
+++ db_image/setup.sh
@@ -9,8 +9,10 @@ psql -c "ALTER SYSTEM SET wal_level TO 'replica';"
 psql -c "ALTER SYSTEM SET wal_log_hints TO 'on';"
 psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
 psql -c "CREATE DATABASE $DB_DATABASE OWNER $DB_USER;"
+sleep 5
 psql -d $DB_DATABASE -a -f /var/lib/postgresql/00_init.sql
+sleep 5
 psql -c "CREATE USER $DB_REPL_USER WITH REPLICATION LOGIN PASSWORD '$DB_REPL_PASSWORD';"
 psql -d $DB_DATABASE -c "ALTER TABLE phones OWNER TO $DB_USER;"
-psql -c $DB_DATABASE -c "ALTER TABLE emails OWNER TO $DB_USER;"
+psql -d $DB_DATABASE -c "ALTER TABLE emails OWNER TO $DB_USER;"
 psql -c "SELECT pg_reload_conf();"
